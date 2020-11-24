8ea74f6eb0399f51a7a899ef844f07627c752845
knlkamal32 <41656240+knlkamal32@users.noreply.github.com>
2020-11-24 16:21:54 +0530

    Delete config.js

--- utils/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    "firstUrl" : "mongodb+srv://john:6QEbiXC6hdez7AMo@cluster0.fwxu0.mongodb.net/user1?retryWrites=true&w=majority",
-    "secondUrl" : "mongodb+srv://john:6QEbiXC6hdez7AMo@cluster0.fwxu0.mongodb.net/user2?retryWrites=true&w=majority"
-}
