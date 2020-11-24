df45ccc55725be7d04fde9b31e2e3abbd8aa551b
knlkamal32 <41656240+knlkamal32@users.noreply.github.com>
2020-11-24 16:21:23 +0530

    Delete database.js

--- utils/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const config = require('./config');
-
-mongoose.Promise = global.Promise;
-const mongoConnect = () => mongoose.connect(config.firstUrl,{useNewUrlParser : true, useUnifiedTopology : true})
-.catch(err => console.log(err))
-
-
-exports.mongoConnect = mongoConnect;
