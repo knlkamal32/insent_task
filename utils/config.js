288ea5586e1385c7a7ea1a04423398361da0e3c4
knlkamal32 <41656240+knlkamal32@users.noreply.github.com>
2020-11-24 16:22:22 +0530

    Add files via upload

---
+++ utils/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "firstUrl" : "mongodb+srv://john:6QEbiXC6hdez7AMo@cluster0.fwxu0.mongodb.net/user1?retryWrites=true&w=majority",
+    "secondUrl" : "mongodb+srv://john:6QEbiXC6hdez7AMo@cluster0.fwxu0.mongodb.net/user2?retryWrites=true&w=majority",
+    "primary" : "mongodb://insent:insent98765@docdb-2020-11-22-13-06-16.c4stmshokttf.ap-southeast-1.docdb.amazonaws.com:27017/user1?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&retryWrites=false",
+    "rsOne" : "mongodb://insent:insent98765@docdb-2020-11-22-13-06-162.c4stmshokttf.ap-southeast-1.docdb.amazonaws.com:27017/user1?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&retryWrites=false",
+    "rsTwo" : "mongodb://insent:insent98765@docdb-2020-11-22-13-06-163.c4stmshokttf.ap-southeast-1.docdb.amazonaws.com:27017/user1?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&retryWrites=false",
+    "rsThree" : "mongodb://insent:insent98765@docdb-2020-11-22-13-06-164.c4stmshokttf.ap-southeast-1.docdb.amazonaws.com:27017/user1?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&retryWrites=false"
+}
